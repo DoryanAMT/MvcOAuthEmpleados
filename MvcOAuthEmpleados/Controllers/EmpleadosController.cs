58b194278e099f9c6b153bad8cd631b9ada1e74d
DoryanAlexandro <doryanamt@gmail.com>
2025-03-28 11:21:15 +0100

    Add project files.

---
+++ MvcOAuthEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcOAuthEmpleados.Filters;
+using MvcOAuthEmpleados.Models;
+using MvcOAuthEmpleados.Services;
+
+namespace MvcOAuthEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+        [AuthorizeEmpleados]
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados = await this.service.GetEmpleadosAsync();
+            return View(empleados);
+        }
+        public async Task<IActionResult> Details
+            (int id)
+        {
+            string token = HttpContext.Session.GetString("TOKEN");
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Debe validarse en el login";
+                return View();
+            }
+            else
+            {
+                Empleado empleado = await
+                    this.service.FindEmpleadoAsync(id, token);
+                return View(empleado);
+            }
+        }
+    }
+}
---
+++ MvcOAuthEmpleados/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MvcOAuthEmpleados.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellido { get; set; }
+        public string Oficio { get; set; }
+        public int Salario { get; set; }
+        public int IdDepartamento { get; set; }
+    }
+}
